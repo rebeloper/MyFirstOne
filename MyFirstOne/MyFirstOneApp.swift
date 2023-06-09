38683af96cfd8f39a487af8b6491b0af80b43fd6
Alex Nagy <rebeloperalpar@gmail.com>
2023-06-09 14:08:28 +0300

    Initial Commit

---
+++ MyFirstOne/MyFirstOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstOneApp.swift
+//  MyFirstOne
+//
+//  Created by Alex Nagy on 09.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
